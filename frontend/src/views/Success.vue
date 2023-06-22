76ffcbbc8b1b434e19c86835455cdfdb061e987d
rawdaGastan <rody.rawda.rr@gmail.com>
2023-06-23 02:59:37 +0300

    format success view

--- frontend/src/views/Success.vue
+++ frontend/src/views/Success.vue
@@ -1,38 +1,38 @@
 <template>
 	<div class="section">
-		 <p class="p">
-	      Thank you for your purchase! If you have any questions, please email
-				<v-col>
-					<a href="mailto:orders@example.com">orders@example.com</a>
-				</v-col>
-	    </p>
+		<p class="p">
+			Thank you for your purchase! If you have any questions, please email
+			<v-col>
+				<a href="mailto:orders@example.com">orders@example.com</a>
+			</v-col>
+		</p>
 	</div>
 </template>
 
 <style>
 .section {
-    background: #ffffff;
-    display: flex;
-    flex-direction: column;
-    width: 400px;
-    border-radius: 6px;
-    justify-content: space-between;
+	background: #ffffff;
+	display: flex;
+	flex-direction: column;
+	width: 400px;
+	border-radius: 6px;
+	justify-content: space-between;
 }
 
 p {
-    font-style: normal;
-    font-weight: 500;
-    font-size: 14px;
-    line-height: 20px;
-    letter-spacing: -0.154px;
-    color: #242d60;
-    height: 100%;
-    width: 100%;
-    padding: 0 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    box-sizing: border-box;
-		padding: 10%;
+	font-style: normal;
+	font-weight: 500;
+	font-size: 14px;
+	line-height: 20px;
+	letter-spacing: -0.154px;
+	color: #242d60;
+	height: 100%;
+	width: 100%;
+	padding: 0 20px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	box-sizing: border-box;
+	padding: 10%;
 }
 </style>
